5431fc9bd518fe43c37c44e9f7000204113c7df2
AnthonyLaiuppa <AnthonyLaiuppa@users.noreply.github.com>
2015-09-10 20:28:15 -0400

    Fixed cron command and added title/text to index

--- loader.sh
+++ loader.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#Install script. Not explaining it because Im going to remove this around alot
 echo ' Hope you have root'
 sudo apt-get install -y python-pip python-flask git 
 sudo pip install flask-bootstrap
@@ -7,6 +8,9 @@ sudo pip install flask-nav
 sudo pip install flask-debug
 sudo pip install flask-wtf
 git clone https://github.com/AnthonyLaiuppa/acataday.git
-1 0 * * * ~/flaskProj/getpic.sh
+crontab -l >mycron 
+echo "01 00 * * * ~/acataday/getpic.sh" >> mycron
+crontab mycron
+rm mycron
 echo 'Scheduled cron to regenerate cat picture at 12:01 everyday' 
 sudo python __init__.py
